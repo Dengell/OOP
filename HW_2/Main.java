8b5872c4428f6dc2db64d9ce7d39ed8079bf6827
Dengell <9680004050@mail.ru>
2023-07-25 10:48:14 +0300

    Do HW_2

---
+++ HW_2/Main.java
@@ -0,0 +1,16 @@
+package HW_2;
+
+import java.util.ArrayList;
+
+import HW_2.Proposal.Product;
+
+
+public class Main {
+    public static void main(String[] args) {
+
+        ArrayList <Product> list_products = new ArrayList<>();
+
+        list_products = new Market().getBase();
+        new MakeOrder().getList_order(list_products);
+    }
+}
