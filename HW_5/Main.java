9bb82fade2d030359ae36f1564d1086178441300
Dengell <9680004050@mail.ru>
2023-08-09 11:20:33 +0300

    DO HW_5

---
+++ HW_5/Main.java
@@ -0,0 +1,12 @@
+package HW_5;
+
+import HW_5.view.UserView;
+
+public class Main {
+
+  public static void main(String[] args) {
+
+    UserView userView = new UserView();
+    userView.addUser();
+  }
+}
